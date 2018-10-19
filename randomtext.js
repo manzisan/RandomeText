95a079ddec682dcd5aab73d03183ed8b89b4075d
manzisan <manzisann@gmail.com>
2018-10-19 22:15:53 +0900

    fix: lint

--- randomtext.js
+++ randomtext.js
@@ -1,24 +1,24 @@
-let nameNum = 0;
+let replaceCount = 0;
 const inName = document.getElementById('inName');
 const nameBox = document.getElementsByClassName('namebox');
-const abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const myName = inName.innerHTML.split("");
 
 inName.innerHTML = '';
 
-myName.map(()=> inName.innerHTML += "<span class='namebox'></span>");
+myName.map(() => inName.innerHTML += "<span class='namebox'></span>");
 
 function randomChange() {
-  myName.map((name, i)=> {
-    let random = Math.floor( Math.random() * abc.length );
-    if (nameBox[nameNum + i]) nameBox[nameNum + i].innerHTML = abc[random];
+  myName.map((name, i) => {
+    let random = Math.floor(Math.random() * abc.length);
+    if (nameBox[replaceCount + i]) nameBox[replaceCount + i].innerHTML = abc[random];
   });
 }
 
 function setName() {
-  let random = Math.floor( Math.random() * myName.length );
-  if (nameBox[nameNum]) nameBox[nameNum].innerHTML = myName[nameNum];
-  nameNum++;
+  let random = Math.floor(Math.random() * myName.length);
+  if (nameBox[replaceCount]) nameBox[replaceCount].innerHTML = myName[replaceCount];
+  replaceCount++;
 }
 
 setInterval(randomChange, 50);
