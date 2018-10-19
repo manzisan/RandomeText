51b8d0078af7155819182d09bcadb39e47243b18
manzisan <manzisann@gmail.com>
2018-10-19 22:12:50 +0900

    fix: es6

--- randomtext.js
+++ randomtext.js
@@ -1,50 +1,25 @@
-window.onload = function() {
+let nameNum = 0;
+const inName = document.getElementById('inName');
+const nameBox = document.getElementsByClassName('namebox');
+const abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const myName = inName.innerHTML.split("");
 
-  var flag = false;
-  var nameNum = 0;
-  var inName = document.getElementById('inName');
-  var nameBox = document.getElementsByClassName('namebox');
-  var abc = new Array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-  var myName = new Array();
+inName.innerHTML = '';
 
-  for (var i = 0; i < inName.innerHTML.length; i++) {
-    myName[i] = inName.innerHTML.charAt(i);
-  }
+myName.map(()=> inName.innerHTML += "<span class='namebox'></span>");
 
-  inName.innerHTML = "";
-
-  for (var i = 0; i < myName.length; i++) {
-    inName.innerHTML += "<span class='namebox'></span>";
-  }
-
-  var randomChange = () => {
-    for (var i = 0; i < myName.length; i++) {
-      var random = Math.floor( Math.random() * abc.length );
-      if(flag) {
-        nameBox[i].innerHTML = abc[random];
-      } else {
-        try {
-          nameBox[nameNum + i].innerHTML = abc[random];
-        } catch(e) {
-          // for no alert
-        }
-      }
-    }
-  }
-
-  var setName = () => {
-    var random = Math.floor( Math.random() * myName.length );
-    nameBox[nameNum].innerHTML = myName[nameNum];
-    nameNum++;
-    // if end of random change logic
-    if (nameNum === myName.length) {
-      flag = "";
-      return;
-    }
-    setTimeout(setName,100);
-  }
-
-  setInterval(randomChange,50);
-  setName();
-
+function randomChange() {
+  myName.map((name, i)=> {
+    let random = Math.floor( Math.random() * abc.length );
+    if (nameBox[nameNum + i]) nameBox[nameNum + i].innerHTML = abc[random];
+  });
 }
+
+function setName() {
+  let random = Math.floor( Math.random() * myName.length );
+  if (nameBox[nameNum]) nameBox[nameNum].innerHTML = myName[nameNum];
+  nameNum++;
+}
+
+setInterval(randomChange, 50);
+setInterval(setName, 100);
